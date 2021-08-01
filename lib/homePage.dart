140a378189d921a245318b4cc0a6a3824bd8a321
M Hasin Bin Sadique <48351876+HasinSadique@users.noreply.github.com>
2021-08-01 13:39:09 +0600

    3rd from Desktop with Homepage Done

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,6 +1,7 @@
 import 'package:circular_profile_avatar/circular_profile_avatar.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class homePage extends StatefulWidget {
   const homePage({Key key}) : super(key: key);
@@ -13,47 +14,168 @@ class _homePageState extends State<homePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        drawer: Drawer(
-          child: ListView(
-            children: [
-              DrawerHeader(
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            Container(
+              height: 180,
+              child: DrawerHeader(
                 decoration: BoxDecoration(
-                  color: Colors.black,
+                  gradient: LinearGradient(
+                      colors: [Color(0xffee0b0b), Color(0xFF8D0C0C)]),
+                ),
+                child: Column(
+                  children: [
+                    Container(
+                      height: 80,
+                      width: 80,
+                      decoration: BoxDecoration(
+                          image: DecorationImage(
+                              image: AssetImage("asset/profilepic.jpg")),
+                          shape: BoxShape.circle,
+                          color: Colors.black),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      "Hasin Sadique",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.favorite,
+                          color: Colors.white,
+                        ),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Text(
+                          "15",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        SizedBox(
+                          width: 30,
+                        ),
+                        Icon(
+                          Icons.star,
+                          color: Colors.white,
+                        ),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Text(
+                          "15.5",
+                          style: TextStyle(color: Colors.white),
+                        )
+                      ],
+                    ),
+                  ],
                 ),
               ),
-              ListTile(
-                leading: Image(
-                  image: AssetImage("asset/education_icon.png"),
-                  height: 30,
-                ),
-                title: Text(
-                  "Education",
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
+            ),
+            ListTile(
+              leading:Icon(Icons.settings,color: Colors.black,),
+              title: Text(
+                "Settings",
+                style: TextStyle(fontWeight: FontWeight.bold),
               ),
-              ListTile(
-                leading: Image(
-                  image: AssetImage("asset/project_logo.png"),
-                  height: 30,
-                  width: 30,
-                ),
-                title: Text(
-                  "Projects",
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
+            ),
+            ListTile(
+              leading: Icon(Icons.logout,color: Colors.black,),
+              title: Text(
+                "Logout",
+                style: TextStyle(fontWeight: FontWeight.bold),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
+      ),
       appBar: AppBar(
         backgroundColor: Color(0xffee0b0b),
         title: Text("Shonjibon"),
         centerTitle: true,
-        actions: [Image(image: AssetImage("asset/msg.png"))],
+        actions: [
+          ElevatedButton(
+            style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Color(0xffee0b0b))),
+            onPressed: () {
+              setState(() {
+                message(context);
+              });
+            },
+            child: Icon(
+              Icons.message,
+              color: Colors.white,
+            ),
+            // child: Image(
+            //     height: 35, width: 40, image: AssetImage("asset/msg.png"))
+          ),
+        ],
       ),
       body: Container(
+        child: DefaultTabController(
+          length: 2,
+          child: Scaffold(
+            appBar: TabBar(
+              indicatorColor: Color(0xffee0b0b),
+              tabs: [
+                Tab(
+                  child: Text(
+                    "News Feed",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  icon: Icon(
+                    Icons.article,
+                    color: Color(0xffee0b0b),
+                  ),
+                ),
+                Tab(
+                    child: Text(
+                      "Notifications",
+                      style: TextStyle(color: Colors.black),
+                    ),
+                    icon: Icon(
+                      Icons.notifications,
+                      color: Color(0xffee0b0b),
+                    )),
+              ],
+            ),
+            body: TabBarView(
+              children: [
+                Column(
+                  children: [
+                    SizedBox(height: 10,),
+                    ElevatedButton(
+                      style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Color(0xffee0b0b))),
+                        onPressed: () {
+                          setState(() {message(context);});
+                        },
+                        child: Text("Post Blood Request",style: TextStyle(fontWeight: FontWeight.bold),))
+                  ],
+                ),
+                Column(
+                  children: [Text("Notifications")],
+                )
+              ],
+            ),
+          ),
+        ),
       ),
-
     );
+  }
+
+  message(BuildContext context) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text("Hello Ji, Pore Kotha Hobe. Bye"),
+    ));
   }
 }
